6737c0f87d0ed082d160598d96b3c1d62887efef
SaraKhamisMostafa <sarakhamismostafa@gmail.com>
2023-01-12 22:44:53 +0200

    code javascript basics

---
+++ code.js
@@ -0,0 +1,17 @@
+console.log("Hello Mystro")
+console.log(4+10)
+
+//-------
+
+//Variables
+
+let name = ' Sara';
+let age = 22;
+let names = ' sara , salma ', ages=20;
+let salary =1000;
+
+if(age>20){
+console.log('Enter')
+}
+
+console.log(name , age, salary);
